b3c0c2e15213bb5e77b59ca560d9d39d19bb5815
helderdsa <helder_dsa@hotmail.com>
2022-05-05 17:09:29 -0300

    requisito 3 completo

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const headerButton = document.querySelector('#button-header');
 const headerEmail = document.querySelector('#email-header');
 const headerPassword = document.querySelector('#password-header');
 
-headerButton.addEventListener(function() {
-
-})
+headerButton.addEventListener('click', () => {
+  if (headerEmail.value === 'tryber@teste.com' && headerPassword.value === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+});
