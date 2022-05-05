64f84e898fcbfeaa8e90eed2719ff4f9b292f7a2
helderdsa <helder_dsa@hotmail.com>
2022-05-05 16:39:42 -0300

    requisito 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const headerButton = document.querySelector('#button-header');
+const headerEmail = document.querySelector('#email-header');
+const headerPassword = document.querySelector('#password-header');
+
+headerButton.addEventListener(function() {
+
+})
