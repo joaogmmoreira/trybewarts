7942963fd57d34b62684886cb01dba93e33ea574
Joao Moreira <jgmoreira1809@gmail.com>
2022-05-06 07:26:37 -0700

    requisitos obrigatorios completos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const headerButton = document.querySelector('#button-header');
 const headerEmail = document.querySelector('#email-header');
 const headerPassword = document.querySelector('#password-header');
+const submitBtn = document.querySelector('#submit-btn');
+const agreement = document.querySelector('#agreement');
 
 headerButton.addEventListener('click', () => {
   if (headerEmail.value === 'tryber@teste.com' && headerPassword.value === '123456') {
@@ -10,3 +12,11 @@ headerButton.addEventListener('click', () => {
   }
 });
 
+submitBtn.disabled = true;
+agreement.addEventListener('click', () => {
+  if (agreement.checked === true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
+  }
+});
