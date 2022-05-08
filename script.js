6010cacc1065806c9dd2567ea80cf5a388ea076a
helderdsa <helder_dsa@hotmail.com>
2022-05-07 21:27:09 -0300

    requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const headerEmail = document.querySelector('#email-header');
 const headerPassword = document.querySelector('#password-header');
 const submitBtn = document.querySelector('#submit-btn');
 const agreement = document.querySelector('#agreement');
+const textarea = document.querySelector('#textarea');
+const counter = document.querySelector('#counter');
 
 headerButton.addEventListener('click', () => {
   if (headerEmail.value === 'tryber@teste.com' && headerPassword.value === '123456') {
@@ -14,9 +16,16 @@ headerButton.addEventListener('click', () => {
 
 submitBtn.disabled = true;
 agreement.addEventListener('click', () => {
-  if (agreement.checked === true) {
-    submitBtn.disabled = false;
-  } else {
-    submitBtn.disabled = true;
-  }
+  submitBtn.disabled = agreement.checked !== true;
+});
+
+textarea.addEventListener('keyup', () => {
+  const lang = textarea.value;
+  console.log(lang.length);
+  counter.innerHTML = 500 - lang.length;
+});
+
+submitBtn.addEventListener('click', () => {
+  const formData = new FormData(document.querySelector('form'));
+  console.log(formData);
 });
