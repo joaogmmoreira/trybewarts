a4f11ce4df84b7abd96f9f2882f465990967ff83
helderdsa <helder_dsa@hotmail.com>
2022-05-09 16:26:20 -0300

    tentativa do requisito 21

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const agreement = document.querySelector('#agreement');
 const textarea = document.querySelector('#textarea');
 const counter = document.querySelector('#counter');
 
-const mainForm = document.querySelector('#main-form-section');
-const formResume = document.querySelector('#form-resume');
-const inputName = document.querySelector('#input-name');
-const inputLastname = document.querySelector('#input-lastname');
-const inputEmail = document.querySelector('#input-email');
-const inputHouse = document.querySelector('#house');
-const inputInterest = document.querySelectorAll('.subject');
-const interestArray = [];
+// const mainForm = document.querySelector('#main-form-section');
+// const formResume = document.querySelector('#form-resume');
+// const inputName = document.querySelector('#input-name');
+// const inputLastname = document.querySelector('#input-lastname');
+// const inputEmail = document.querySelector('#input-email');
+// const inputHouse = document.querySelector('#house');
+// const inputInterest = document.querySelectorAll('.subject');
+// const interestArray = [];
 
 headerButton.addEventListener('click', () => {
   if (headerEmail.value === 'tryber@teste.com' && headerPassword.value === '123456') {
@@ -34,31 +34,31 @@ textarea.addEventListener('keyup', () => {
   counter.innerHTML = 500 - lang.length;
 });
 
-const showElement = (element) => { element.style.display = 'flex'; };
-const hiddenElement = (element) => { element.style.display = 'none'; };
+// const showElement = (element) => { element.style.display = 'flex'; };
+// const hiddenElement = (element) => { element.style.display = 'none'; };
 
-function interestCheck() {
-  for (interest of inputInterest) {
-    if(interest.checked) {
-      interestArray.push(interest.value+' ');
-    };
-  };
-};
+// function interestCheck() {
+//   for (interest of inputInterest) {
+//     if(interest.checked) {
+//       interestArray.push(interest.value+' ');
+//     };
+//   };
+// };
 
-submitBtn.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  const rate = document.querySelector('input[type="radio"][name=rate]:checked').value;
-  const family = document.querySelector('input[type="radio"][name=family]:checked').value;
-  interestCheck();
-  hiddenElement(mainForm);
-  showElement(formResume);
-  console.log(
-    inputName.value,
-    inputLastname.value,
-    inputEmail.value,
-    inputHouse.value,
-    rate,
-    family,
-    interestArray,
-  );
-});
+// submitBtn.addEventListener('click', (evt) => {
+//   evt.preventDefault();
+//   const rate = document.querySelector('input[type="radio"][name=rate]:checked').value;
+//   const family = document.querySelector('input[type="radio"][name=family]:checked').value;
+//   interestCheck();
+//   hiddenElement(mainForm);
+//   showElement(formResume);
+//   console.log(
+//     inputName.value,
+//     inputLastname.value,
+//     inputEmail.value,
+//     inputHouse.value,
+//     rate,
+//     family,
+//     interestArray,
+//   );
+// });
